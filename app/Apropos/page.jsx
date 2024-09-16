aac757621e3d3e68851cbc6c5e0e0fc489b8efad
aboub95 <messireabou33@gmail.com>
2024-09-16 16:16:29 +0100

    First commit by Abou

--- app/Apropos/page.jsx
+++ app/Apropos/page.jsx
@@ -2,7 +2,7 @@ export default function page() {
   return (
     <div>
       <h1 className="text-justify font-bold bg-red-600 flex justify-center items-center">
-        Bienvenu sur la page A propos...!
+        Bienvenu sur la page A propos...! Gatien me parle fort....!
       </h1>
     </div>
   );
